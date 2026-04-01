da1faf5d054cc083bedb5664ecb21e568ab38261
Aleksej Mosunov <alexmosunov8@gmail.com>
2026-04-01 15:26:57 +0300

    fix(backend): use '/*' for OPTIONS route to avoid PathError

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -41,7 +41,7 @@ app.use(
   }),
 );
 app.options(
-  "*",
+  "/*",
   cors({
     origin: process.env.FRONTEND_URL || "http://localhost:3010",
     methods: ["GET", "POST", "DELETE", "PATCH", "PUT", "OPTIONS"],
